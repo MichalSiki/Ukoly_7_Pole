b12c6dbc0cbb48eecc1d1374669c82082f4cc2ef
MichalS <99993669+MichalSiki@users.noreply.github.com>
2022-05-12 11:42:42 +0200

    Ukoly_7_Pole

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String[] zelenina = {"zeli" , "okurka" , "rajce" , "paprika" , "mrkev"};
+        String[] ovoce = {"jablko" , "hruska" , "pomeranc" , "jahoda" , "banan"};
+        System.out.println("Zadej nazev ovoce nebo zeleniny");
+        String nazev = sc.nextLine();
+        if (nazev = ovoce) {
+            System.out.println("Tohle je ovoce");
+        }
+            else if (nazev = zelenina){
+            System.out.println("Tohle je zelenina");
+        }
+            else {
+            System.out.println("Neznam");
+        }
     }
 }
