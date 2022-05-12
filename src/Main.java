e8befe382c5454e867dd056b5b97e8ac51bbbb2b
MichalS <99993669+MichalSiki@users.noreply.github.com>
2022-05-12 13:10:01 +0200

    Ukoly_7_Pole

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -6,14 +8,20 @@ public class Main {
         String[] ovoce = {"jablko" , "hruska" , "pomeranc" , "jahoda" , "banan"};
         System.out.println("Zadej nazev ovoce nebo zeleniny");
         String nazev = sc.nextLine();
-        if (nazev = ovoce) {
-            System.out.println("Tohle je ovoce");
+        Arrays.sort(zelenina);
+        Arrays.sort(ovoce);
+        int poziceZ = Arrays.binarySearch(zelenina, nazev);
+        int poziceO = Arrays.binarySearch(ovoce, nazev);
+        System.out.println(poziceZ);
+        System.out.println(poziceO);
+        if (poziceZ>= 0 ){
+            System.out.println("To je zelenina");
         }
-            else if (nazev = zelenina){
-            System.out.println("Tohle je zelenina");
+        else if (poziceO>= 0){
+            System.out.println("To je ovoce");
         }
-            else {
-            System.out.println("Neznam");
+        else{
+            System.out.println("neznam");
         }
     }
 }
